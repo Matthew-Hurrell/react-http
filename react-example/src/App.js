97e8193d902944d05888067b3a93ff4efc53780c
Matthew Hobbs-Hurrell <matthewhobbshurrell@icloud.com>
2023-03-29 19:47:01 +0000

    Add HTTPHooks js file with logic

--- react-example/src/App.js
+++ react-example/src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import HTTPRequest from './components/HTTPRequest';
 import HTTPPost from './components/HTTPPost';
+import HTTPHooks from './components/HTTPHooks';
 
 function App() {
   return (
     <div className="App">
       {/* <HTTPRequest /> */}
-      <HTTPPost />
+      {/* <HTTPPost /> */}
+      <HTTPHooks />
     </div>
   );
 }
